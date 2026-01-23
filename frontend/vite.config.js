9272e249be64868efdc22e3f8f5abb06ee8ad461
Dudoladov Dmitry <dudmitry.iv@gmail.com>
2026-01-23 20:57:43 +0300

    chore: update frontend host to город-решений.рф

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,25 +3,29 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: '0.0.0.0',          // слушаем на всех интерфейсах (Tailscale и т.д.)
-    strictPort: true,
-    allowedHosts: ['higu.su, .higu.su, server.higu.su', '.behired.ru'],
+    plugins: [react()],
+    server: {
+        host: '0.0.0.0',          // слушаем на всех интерфейсах (Tailscale и т.д.)
+        strictPort: true,
+        allowedHosts: [
+            'xn----etbdfash4aeavd9h.xn--p1ai',
+            'www.xn----etbdfash4aeavd9h.xn--p1ai',
+            'город-решений.рф', 
+            'www.город-решений.рф'],
 
-    hmr: {
-      https: false,
-      host: 'higu.su',     // именно твой домен
-      // protocol: 'wss',        // обязательно wss, потому что Nginx Proxy Manager terminates TLS
+        hmr: {
+            https: false,
+            host: 'город-решений.рф',     // именно твой домен
+            // protocol: 'wss',        // обязательно wss, потому что Nginx Proxy Manager terminates TLS
+        },
+        proxy: {
+            // Все запросы, которые начинаются с /api,
+            // будут перенаправлены на твой бэкенд
+            '/api': {
+                target: 'http://сервер.город-решений.рф',
+                changeOrigin: true,           // меняет заголовок Host на target
+                secure: false,                // если http, а не https
+            }
+        }
     },
-    proxy: {
-      // Все запросы, которые начинаются с /api,
-      // будут перенаправлены на твой бэкенд
-      '/api': {
-        target: 'http://server.higu.su',
-        changeOrigin: true,           // меняет заголовок Host на target
-        secure: false,                // если http, а не https
-      }
-    }
-  },
 })
